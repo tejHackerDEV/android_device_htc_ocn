80da44cfe5f24c5bba88541bf1f30a0eeac6d6b0
tejHackerDev <tejasimha222@gmail.com>
2023-10-15 14:28:20 +0530

    ocn: Bringup for `CarbonRom`
    Signed-off-by: tejHackerDev <tejasimha222@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocn.mk
+    $(LOCAL_DIR)/carbon_ocn.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocn-user \
-    lineage_ocn-userdebug \
-    lineage_ocn-eng
+    carbon_ocn-user \
+    carbon_ocn-userdebug \
+    carbon_ocn-eng
