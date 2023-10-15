033f771cc30103273afc137f73d288e55932dbfe
tejHackerDev <tejasimha222@gmail.com>
2023-10-15 14:45:31 +0530

    ocn: Remove `LineageTouch` hal
    Signed-off-by: tejHackerDev <tejasimha222@gmail.com>

--- device.mk
+++ device.mk
@@ -338,10 +338,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.ocn
 
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.ocn
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
