653bc040250d6f3d3520d082643f86c333b1613a
tejHackerDev <tejasimha222@gmail.com>
2023-10-19 20:02:52 +0530

    ocn: Build missing `soundtrigger` entries
    hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.3::ISoundTriggerHw/default in either framework or device manifest.
    LegacySupport: Could not get passthrough implementation for android.hardware.soundtrigger@2.3::ISoundTriggerHw/default.
    
    Signed-off-by: tejHackerDev <tejasimha222@gmail.com>

--- device.mk
+++ device.mk
@@ -115,7 +115,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.primary.msm8998 \
     audio.r_submix.default \
